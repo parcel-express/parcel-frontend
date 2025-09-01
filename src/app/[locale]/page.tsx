22b79f13d7cdd177f5c1a0ebbdd1a4d2bedcf89d
Nini Gordiashvili <gordiashvilinini@gmail.com>
2025-09-01 16:43:07 +0400

    created Container and Responsive

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,59 +1,18 @@
 'use client';
 
-import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 import styled from 'styled-components';
 
-import { Button, Heading1, BodyLarge } from '../../components';
+import Container from '@/components/Container';
 
-const Container = styled.div`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 2rem;
+const LogoImage = styled(Image)`
+  object-fit: contain;
 `;
 
-const Title = styled(Heading1)`
-  margin-bottom: 1rem;
-  text-align: center;
-`;
-
-const Description = styled(BodyLarge)`
-  text-align: center;
-  color: #535862;
-  margin-bottom: 2rem;
-`;
-
-const Navigation = styled.nav`
-  display: flex;
-  gap: 2rem;
-  margin-top: 2rem;
-`;
-
-export default function Home() {
-  const t = useTranslations('HomePage');
-  const navT = useTranslations('Navigation');
-
+export default function Page() {
   return (
     <Container>
-      <Title>{t('title')}</Title>
-      <Description>{t('description')}</Description>
-
-      <Navigation>
-        <Button variant='primary' size='md'>
-          {navT('home')}
-        </Button>
-        <Button variant='secondary' size='md'>
-          {navT('about')}
-        </Button>
-        <Button variant='tertiary' size='md'>
-          {navT('services')}
-        </Button>
-        <Button variant='disabled' size='md' disabled>
-          {navT('contact')}
-        </Button>
-      </Navigation>
+      <LogoImage src='/images/logo.png' alt='logo' width={84} height={84} />
     </Container>
   );
 }
---
+++ src/components/Container.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const StyledContainer = styled.div`
+  max-width: 1320px;
+  margin: 0 auto;
+  padding: 0 60px;
+  @media (max-width: 1080px) {
+    max-width: 100%;
+    padding: 0 20px;
+  }
+`;
+
+const Container: React.FC<Props> = ({ children }) => {
+  return <StyledContainer>{children}</StyledContainer>;
+};
+
+export default Container;
