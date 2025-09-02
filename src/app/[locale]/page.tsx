e9d40650e76fb3b713ce649c89ce61353b5332df
ninigordiashvili <gordiashvilinini@gmail.com>
2025-09-02 14:59:48 +0400

    created footer web & mobile (#4)

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,12 @@
 'use client';
+import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
 export default function Page() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
